9bd36abacf4e86b5da820ef326e6b49b99d4f844
jvacca <ataribro@hotmail.com>
2024-05-17 13:42:08 -0400

    fixing tooltip

--- src/components/UICommon/ToolTip.jsx
+++ src/components/UICommon/ToolTip.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import parse from 'html-react-parser';
 import styles from './ToolTip.module.scss';
 
-export default function ToolTip({show, copy}) {
+export default function ToolTip({show, children}) {
   
   return (
     <>
     {(show) &&
       <div className={styles.tooltip}>
         <div className={styles.container}>
-          <p>{parse(copy)}</p>
+          {children}
         </div>
         <div className={styles.triangle}></div>
       </div> 
